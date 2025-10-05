96d83c07ed2d0340f84b873a198427c147130371
Diego Fischer <diegof01gm@gmail.com>
2025-10-05 19:24:32 -0300

    Agrego imágenes, estilos y página de alojamientos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return <></>;
+import { Routes, Route } from 'react-router-dom';
+import PaginaAlojamientos from './pages/PaginaAlojamientos';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Inicio de Viaggio</h1>} />
+      <Route path="/alojamientos" element={<PaginaAlojamientos />} />
+    </Routes>
+  );
 }
+
+export default App;
