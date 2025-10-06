86eaf64640d8ed9cd6cc9bc6d8fbbbf13a5bed56
Diego Fischer <diegof01gm@gmail.com>
2025-10-06 20:17:58 -0300

    ruteo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
+import Alojamientos from "./pages/PaginaAlojamientos";
 import Destinos from "./pages/Destinos";
+import PaginaAlojamientos from "./pages/PaginaAlojamientos";
 
 export default function App() {
-  return (<Destinos />)
+  return (<PaginaAlojamientos />)
 };
